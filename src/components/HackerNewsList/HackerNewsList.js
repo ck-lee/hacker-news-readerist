641149909b2044ed8d35cecd1bd39b520236e89f
Chao Koon Lee <chaokoon@gmail.com>
2018-09-01 08:30:10 +1200

    Fix lint.

--- src/components/HackerNewsList/HackerNewsList.js
+++ src/components/HackerNewsList/HackerNewsList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
+import PropTypes from 'prop-types';
 
 const HackerNewsList = ({ children }) => (
   <div>
     <ul>{children}</ul>
   </div>
 );
+
+HackerNewsList.propTypes = {
+  children: PropTypes.array.isRequired,
+};
 
 export default HackerNewsList;
--- src/containers/HackerNewsContainer.js
+++ src/containers/HackerNewsContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies */
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import HackerNewsList from '../components/HackerNewsList/HackerNewsList';
@@ -6,13 +7,13 @@ import HackerNewsList from '../components/HackerNewsList/HackerNewsList';
 const HackerNewsContainer = ({ newsIds }) => (
   <HackerNewsList>
     {
-      newsIds.map(newsId => <li key={newsId}>{newsId}</li>,)
+      newsIds.map(newsId => <li key={newsId}>{newsId}</li>)
     }
   </HackerNewsList>
 );
 
 HackerNewsContainer.propTypes = {
-  newsIds: PropTypes.array,
+  newsIds: PropTypes.array.isRequired,
 };
 
 const mapStateToProps = state => ({
