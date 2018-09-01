c504d9637fae6af8630e0710f4a87873cae14b4d
Chao Koon Lee <chaokoon@gmail.com>
2018-09-02 02:43:09 +1200

    Implemented PWA.

--- src/api/hackerNews.js
+++ src/api/hackerNews.js
@@ -1,6 +1,19 @@
-const getNewStories = () => fetch('https://hacker-news.firebaseio.com/v0/newstories.json');
+const version = '0.1.0';
+const cacheName = `hacker-news-readerist-${version}`;
 
-const getItem = id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+const getNewStories = () => fetch('https://hacker-news.firebaseio.com/v0/newstories.json').then((response) => {
+  return caches.open(cacheName).then((cache) => {
+    cache.put('https://hacker-news.firebaseio.com/v0/newstories.json', response.clone());
+    return response;
+  });
+});
+
+const getItem = id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then((response) => {
+  return caches.open(cacheName).then((cache) => {
+    cache.put(`https://hacker-news.firebaseio.com/v0/item/${id}.json`, response.clone());
+    return response;
+  });
+});
 
 export default {
   getItem,
